a1bdb5a9d3670c28bc47dc3d7ffd5313a58980c2
akukamil <chamilioner_kid@mail.ru>
2021-04-30 19:32:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2491,6 +2491,7 @@ function load_yandex() {
 				player = _player;  
 								
 				my_data.first_name = player.getName();
+				console.log(my_data.first_name);
 				my_data.uid = player.getUniqueID();
 				my_data.uid = my_data.uid.replace("/", "$");				
 				my_data.pic_url=player.getPhoto('medium');				
