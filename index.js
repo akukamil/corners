d6bc652ed543e94655e893402f85af7250ba568a
akukamil <chamilioner_kid@mail.ru>
2021-10-21 16:34:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3438,19 +3438,6 @@ var auth = function() {
 			  })
 			},
 
-			vkbridge_events: function(e) {
-
-				if (e.detail.type === 'VKWebAppGetUserInfoResult') {
-
-					my_data.name 	= e.detail.data.first_name + ' ' + e.detail.data.last_name;
-					my_data.uid 	= "vk"+e.detail.data.id;
-					my_data.pic_url = e.detail.data.photo_100;
-
-					//console.log(`Получены данные игрока от VB MINIAPP:\nимя:${my_data.name}\nid:${my_data.uid}\npic_url:${my_data.pic_url}`);
-					help_obj.process_results();
-				}
-			},
-
 			init: function() {
 
 				g_process=function() { help_obj.process()};
@@ -3459,6 +3446,7 @@ var auth = function() {
 
 				//-----------ЯНДЕКС------------------------------------
 				if (s.includes("yandex")) {
+					game_platform="YANDEX";
 					Promise.all([
 						this.loadScript('https://yandex.ru/games/sdk/v2')
 					]).then(function(){
@@ -3470,6 +3458,7 @@ var auth = function() {
 
 				//-----------ВКОНТАКТЕ------------------------------------
 				if (s.includes("vk.com")) {
+					game_platform="VK";
 					Promise.all([
 						this.loadScript('https://vk.com/js/api/xd_connection.js?2'),
 						this.loadScript('//ad.mail.ru/static/admanhtml/rbadman-html5.min.js'),
@@ -3485,19 +3474,21 @@ var auth = function() {
 
 				//-----------ЛОКАЛЬНЫЙ СЕРВЕР--------------------------------
 				if (s.includes("192.168")) {
+					game_platform="debug";
 					help_obj.debug();
 					return;
 				}
 
 
 				//-----------НЕИЗВЕСТНОЕ ОКРУЖЕНИЕ---------------------------
+				game_platform="unknown";
 				help_obj.unknown();
 
 			},
 
 			yandex: function() {
 
-				game_platform="YANDEX";
+				
 				if(typeof(YaGames)==='undefined')
 				{
 					help_obj.local();
@@ -3539,16 +3530,29 @@ var auth = function() {
 
 			vk: function() {
 
-				game_platform="VK";
-				vkBridge.subscribe((e) => this.vkbridge_events(e));
-				vkBridge.send('VKWebAppInit');
-				vkBridge.send('VKWebAppGetUserInfo');
+				vkBridge.send('VKWebAppInit').then(()=>{
+					
+					return vkBridge.send('VKWebAppGetUserInfo');
+					
+				}).then((e)=>{
+					
+					my_data.name 	= e.first_name + ' ' + e.last_name;
+					my_data.uid 	= "vk"+e.id;
+					my_data.pic_url = e.photo_100;
+
+					//console.log(`Получены данные игрока от VB MINIAPP:\nимя:${my_data.name}\nid:${my_data.uid}\npic_url:${my_data.pic_url}`);
+					help_obj.process_results();		
+					
+				}).catch(function(e){
+					
+					alert(e);
+					
+				});
 
 			},
 
 			debug: function() {
 
-				game_platform = "debug";
 				let uid = prompt('Отладка. Введите ID', 100);
 
 				my_data.name = my_data.uid = "debug" + uid;
@@ -3559,8 +3563,6 @@ var auth = function() {
 			},
 
 			local: function(repeat = 0) {
-
-				game_platform="YANDEX";
 
 				//ищем в локальном хранилище
 				let local_uid = localStorage.getItem('uid');
@@ -3618,7 +3620,6 @@ var auth = function() {
 					})	
 
 				}
-
 
 			},
 
